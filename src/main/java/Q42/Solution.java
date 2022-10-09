ad7be074ce8b5092ab1fc517e8f16ff920da068c
Cyankyo <cyankyo@163.com>
2022-10-09 14:20:21 +0800

    42 解决

--- src/main/java/Q42/Solution.java
+++ src/main/java/Q42/Solution.java
@@ -1,54 +1,63 @@
-///*
-// * Project: LeetCode
-// *
-// * File Created at 2022/2/25
-// *
-// * Copyright 2016 cyan Corporation Limited.
-// * All rights reserved.
-// *
-// * This software is the confidential and proprietary information of
-// * cyan Company. ("Confidential Information").  You shall not
-// * disclose such Confidential Information and shall use it only in
-// * accordance with the terms of the license.
-// */
-//
-//package Q42;
-//
-///***
-// * @Type
-// * @Desc
-// * @Author cyankyo
-// * @Date 2022/2/25 17:55
-// * @Version 1.0
-// */
-//public class Solution {
-//    public int trap(int[] height) {
-//        int leftMax = 0;
-//        int rightMax = 0;
-//        int left = 0;
-//        int right = height.length - 1;
-//        int size = 0;
-//        while (left != right) {
-//            leftMax = Math.max(leftMax, height[left]);
-//            rightMax = Math.max(rightMax, height[right]);
-//            int max = Math.max(leftMax, rightMax);
-//            if (height[left] > height[right]) {
-//                size += max - height[right];
-//                right++;
-//            }
-//        }
-//    }
-//
-//    public static void main(String[] args) {
-//
-//    }
-//}
-//
-///*
-// * Revision history
-// * -------------------------------------------------------------------------
-// *
-// * Date Author Note
-// * -------------------------------------------------------------------------
-// * 2022/2/25 cyankyo create
-// */
+/*
+ * Project: LeetCode
+ *
+ * File Created at 2022/2/25
+ *
+ * Copyright 2016 cyan Corporation Limited.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * cyan Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license.
+ */
+
+package Q42;
+
+/***
+ * @Type
+ * @Desc
+ * @Author cyankyo
+ * @Date 2022/2/25 17:55
+ * @Version 1.0
+ */
+public class Solution {
+    public int trap(int[] height) {
+        int leftMax = 0;
+        int rightMax = 0;
+        int left = 0;
+        int right = height.length - 1;
+        int size = 0;
+        while (left < right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+            int min = Math.min(leftMax, rightMax);
+            if (leftMax <= rightMax) {
+                if (height[left] < min) {
+                    size = size + min - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] < min) {
+                    size = size + min - height[right];
+                }
+                right--;
+            }
+        }
+        return size;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{4,2,0,3,2,5};
+        System.out.println(new Solution().trap(a));
+    }
+}
+
+/*
+ * Revision history
+ * -------------------------------------------------------------------------
+ *
+ * Date Author Note
+ * -------------------------------------------------------------------------
+ * 2022/2/25 cyankyo create
+ */
